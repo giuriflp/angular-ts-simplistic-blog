bf7d777b7187ee23e2ab774ddd969aa9512873f9
giuriflp <giuriflip@gmail.com>
2022-12-08 14:46:09 -0300

    uploadig components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'life-cycle';
+  title = 'modulos-angular';
 }
---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,34 @@
+export const dataFake = [
+  {
+    id: '1',
+    title: 'Rottweiler',
+    description:
+      'Robusto, forte e imponente, o rottweiler carrega a fama de ser bravo por seu porte intimidador, mas na verdade é apenas um cachorro com grande instinto de proteção e lealdade ao seu tutor. O cão dessa raça pode ser dócil, carinhoso e ótimo companheiro para a família, quando treinado.',
+    photoCover:
+      'https://love.doghero.com.br/wp-content/uploads/2018/02/shutterstock_378336934-768x510.webp',
+  },
+  {
+    id: '2',
+    title: 'Husky Siberiano',
+    description:
+      'À primeira vista, sua cara de lobo e seu porte podem ser intimidadores. Mas, na verdade, o husky siberiano é amigável, gentil e sociável, além de muito trabalhador. Também conhecida como cão de trenó, essa raça foi criada com a companhia de humanos e outros animais para transportar cargas e pessoas, e ter uma convivência pacífica com todos. Isso resultou não só em sua impressionante resistência física e vontade de trabalhar, mas também em um temperamento agradável, de boa convivência e muito extrovertido.',
+    photoCover:
+      'https://love.doghero.com.br/wp-content/uploads/2018/08/husky-siberiano-2.webp',
+  },
+  {
+    id: '3',
+    title: 'Rough Collie',
+    description:
+      'Ótimo companheiro para toda a família, o cachorro Collie, também conhecido como Rough Collie,  é muito famoso, e claro, não podemos deixar de citar que um dos motivos seja porque essa é a raça da Lassie, famosa cachorrinha do livro “A Força do Coração”, e que foi estrela de diversos filmes e séries ao longo do século XX. Outro ponto importante que torna a raça tão querida, é o fato dela ser tão carismática, leal e inteligente. ',
+    photoCover:
+      'https://love.doghero.com.br/wp-content/uploads/2021/10/pexels-kanashi-5957235-768x512.jpg',
+  },
+  {
+    id: '4',
+    title: 'Dogue Alemão',
+    description:
+      'Também conhecido como Apolo dos Cachorros ou dinamarquês, o dogue alemão é uma das raças mais altas do mundo. Muitas vezes esse cachorro se torna mais alto que um humano adulto ao ficar em pé sobre duas patas. No dia a dia, é muito divertido e vai fazer você rir em diversas situações, pois não tem noção de seu tamanho e força – e acredita que cabe perfeitamente no colo de qualquer pessoa.',
+    photoCover:
+      'https://love.doghero.com.br/wp-content/uploads/2018/03/shutterstock_55480915-e1541428990781-768x576.webp',
+  },
+];
